cf02f8a47754f6bd017b2fead39a638af17e5958
Victor Volkov <vityan2504@gmail.com>
2022-09-01 21:42:31 +0300

    написал три проверки на вывод дней недели

---
+++ script.js
@@ -0,0 +1,40 @@
+const lang = 'en';
+
+const date = [
+   [
+      'Воскресенье',
+      'Понедельник',
+      'Вторник',
+      'Среда',
+      'Четверг',
+      'Пятница',
+      'Суббота'
+   ],
+   [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'
+   ]
+]
+
+if (lang === 'ru') {
+   console.log(date[0].join(', '));
+} 
+if (lang === 'en') {
+   console.log(date[1].join(', '));
+}
+
+switch (lang) {
+   case 'ru':
+      console.log(date[0].join(', '));
+      break;
+   case 'en':
+      console.log(date[1].join(', '));
+      break;
+}
+
+lang === 'ru' ? console.log(date[0].join(', ')) : console.log(date[1].join(', '));
